e28a87e7d856aa1f43394b030f1b8f5858e0573b
Silberdinho <aarons@uia.no>
2026-02-19 16:11:59 +0100

    fikset scrolle greier

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import OrderConfirmation from "./pages/OrderConfirmation";
 import NotFound from "./pages/NotFound";
 import { CartProvider } from "@/context/CartContext";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
 
 const queryClient = new QueryClient();
 
@@ -17,6 +26,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <HashRouter>
+        <ScrollToTop />
         <CartProvider>
           <Routes>
             <Route path="/" element={<Index />} />
